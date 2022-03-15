7ac71cbb17672c5571b6b7fdd8fb2e7cb4dc4e94
Utsav Shrestha <utsav.shrestha@mavs.uta.edu>
2022-03-15 10:38:03 -0500

    added mongoose

--- app chat/server.js
+++ app chat/server.js
@@ -8,23 +8,42 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
+var dbUrl = 'mongodb://user:user@ds155424.mlab.com:55425/learning-node'
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
+
 var messages = [
     {name: 'Tim', message: 'Hi'},
     {name: 'Jane', message: 'Hello'}
 ]
 
 app.get('/messages', (req, res) =>{
-    res.send(messages)
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
 })
-
+ 
 app.post('/messages', (req, res) =>{
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body)
+
+    message.save((err) => {
+        if(err)
+            sendStatus(500)
+         messages.push(req.body)
+        io.emit('message', req.body)
+        res.sendStatus(200)
+    })
 })
 
 io.on('connection', (socket) => {
     console.log('a user connected')
+})
+
+mongoose.connect(dbUrl, {useMongoClient: true}, (err) => {
+    console.log('mongo db connection', err)
 })
 
 var server = http.listen(3000, () => {
